aae1aa4441456a1c3a9d381fa131b402198805f4
sharada113 <dundretaurora@yahoo.com>
2023-09-15 07:42:15 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -202,8 +202,8 @@ resource "null_resource" "configure-cat-app" {
   }
 }
 
-module "azure-backup" {
+/*module "azure-backup" {
   source  = "ravensorb/azure-backup/azurerm"
   version = "1.0.2"
   resource_group_name = "${var.prefix}-public"
-}
+}*/
